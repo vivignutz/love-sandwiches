87d34c7d250915db5483423c9ca7299dc109bfb4
vivignutz <vgnutzmann@gmail.com>
2022-11-24 16:19:12 +0000

    Adjust validation to check that all values provide integers

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#Your code goes here
+#You can delete these comments, but do not change the name of this file
+#Write your code to expect a terminal of 80 characters wide and 24 rows high
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -24,15 +24,14 @@ def get_sales_data():
     print("Data should be six numbers, separated by commas.")
     print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here: ")
-    #print(f"The data provided is {data_str}")
+    data_str = input("Enter your data here: ") #print(f"The data provided is {data_str}")
     """
     Use input() method to get sales data  from the user in the terminal
     """
 
     sales_data = data_str.split(",") #split() method returns the broken up values as a lsit
     validate_data(sales_data) 
-
+    
 #create a new function to handle our validation:
 def validate_data(values):
     """
@@ -40,7 +39,9 @@ def validate_data(values):
     Raises ValueError of strings cannot be converted into Int,
     or if there arent exactly 6 values.
     """
+    print(values)
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
@@ -49,7 +50,8 @@ def validate_data(values):
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
 
-get_sales_data() 
+get_sales_data()
 #RESULTADO: os values tem q estar numa lista:
 #Enter your data here: 1,2,3,4,5,6,5
-#['1', '2', '3', '4', '5', '6', '5']
+#['1', '2', '3', '4', '5', '6', '5'] td o q tá entre apóstrofe é string
+
